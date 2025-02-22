140c329366b7fb80c73b23d0b1df4ca5e7cf5dc9
EmanuER <germyruiz10@gmail.com>
2025-02-22 02:05:02 -0600

    Finaliza el problema 1: Este es un agente de semáforo

--- Problema_1/Agente_Semaforo_Inteligente_Reactivo.py
+++ Problema_1/Agente_Semaforo_Inteligente_Reactivo.py
@@ -5,7 +5,7 @@ from threading import Thread, Event
 class AgenteSemaforo:
     def __init__(self):
         self.estado = "Rojo"  # Esta en Rojo del Semaforo
-        self.tiempo_verde = 10  # Tiempo en verde (cuando inicia)
+        self.tiempo_verde = 15  # Tiempo en verde (cuando inicia)
         self.tiempo_amarillo = 3  # Tiempo en amarillo (Intermedio)
         self.tiempo_rojo = 5  # Tiempo en rojo (Para y deteccion)
         self.evento_trafico = Event()  # Evento para poder simular los carros o trafico
@@ -13,23 +13,60 @@ class AgenteSemaforo:
 
     def detectar_vehiculos(self):
         """Se inicia la simulación para detectar los vehículos"""
-        #Logica para la deteccion de autos
-        pass
+        while True:
+            time.sleep(2)  # Dos segundos para dejar la simulación
+            self.vehiculos_detectados = random.randint(0, 20)
+            print(f"Vehículos detectados: {self.vehiculos_detectados}")
+            if self.vehiculos_detectados > 15:
+                self.evento_trafico.set()  # Se habilita el evento de trafico intenso
+            else:
+                self.evento_trafico.clear()  # Se desactiva el evento y sigue detectando
 
     def ajustar_tiempos(self):
         """Se hace el ajuste del tiempo dependiendo la cantidad de vehículos"""
-        # Logica para los tiempos.
-        pass
+        if self.vehiculos_detectados > 15:
+            self.tiempo_verde = 20  # Si hay mucho tráfico, mas tiempo en verde
+        elif self.vehiculos_detectados > 5:
+            self.tiempo_verde = 15  # Se usa el tiempo normal en verde
+        else:
+            self.tiempo_verde = 8  # Si hay poco trafico, menos tiempo en verde
 
     def cambiar_estado(self):
         """Se cambia el estado del semáforo"""
-        # Logica para el cambio de estado del semaforo
-        pass
+        while True:
+            if self.estado == "Verde":
+                self.estado = "Amarillo"
+                tiempo_espera = self.tiempo_amarillo
+            elif self.estado == "Amarillo":
+                self.estado = "Rojo"
+                tiempo_espera = self.tiempo_rojo
+            else:
+                self.estado = "Verde"
+                tiempo_espera = self.tiempo_verde
+
+            print(f"El semáforo está en {self.estado} → tiempo: {tiempo_espera} segundos.")
+            time.sleep(tiempo_espera)
+
+            # Se ajusta el tiempo, si se activa el evento del trafico
+            if self.evento_trafico.is_set():
+                self.ajustar_tiempos()
+                print("Ajustando de tiempo por trafico intenso")
 
     def ejecutar(self):
         """Acá se ejecuta el agente del semáforo"""
-        # Logica para que funcione el agente
-        pass
+        # Este hilo detecta los vehículos
+        hilo_deteccion = Thread(target=self.detectar_vehiculos)
+        hilo_deteccion.daemon = True  # Si el programa termina el hilo se detiene
+        hilo_deteccion.start()
+
+        # Se cambia el estado del semáforo
+        hilo_semaforo = Thread(target=self.cambiar_estado)
+        hilo_semaforo.daemon = True
+        hilo_semaforo.start()
+
+        # While para mantener el programa en bucle
+        while True:
+            time.sleep(1)
 
 # Creación y ejecución del agente
 semaforo = AgenteSemaforo()
