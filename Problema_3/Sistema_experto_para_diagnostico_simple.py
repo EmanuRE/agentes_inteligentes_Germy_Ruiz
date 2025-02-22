718a471f2b878b7c6a67d61ed931e093f33501b1
EmanuER <germyruiz10@gmail.com>
2025-02-22 02:13:22 -0600

    Inicia el problema 3: Un sistema experto para diagnostico simple

---
+++ Problema_3/Sistema_experto_para_diagnostico_simple.py
@@ -0,0 +1,17 @@
+def solicitar_sintomas():
+    """Se solicita al usuario ingresar su sintomas"""
+    # Logica para solicitar los sintomas
+    pass
+
+def diagnosticar(sintomas):
+    """Se realiza un diagnóstico basado en los sintomas"""
+    # Logica para el diagnostico
+    pass
+
+def ejecutar_sistema_experto():
+    """Se ejecuta el sistema para el diagnóstico"""
+    # Logica para ejecutar todo el sistema
+    pass
+
+# Se ejecuta el sistema
+ejecutar_sistema_experto()
