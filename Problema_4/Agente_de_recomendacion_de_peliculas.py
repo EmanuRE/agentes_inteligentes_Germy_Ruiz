9e4674c4ffc7f1d29ce8e22c2697c777ce300002
EmanuER <germyruiz10@gmail.com>
2025-02-22 02:15:55 -0600

    Inicia el problema 3: Agente para recomendar peliculas

---
+++ Problema_4/Agente_de_recomendacion_de_peliculas.py
@@ -0,0 +1,24 @@
+import random
+
+def cargar_peliculas():
+    """Diccionario de peliculas organizadas por genero"""
+    # Logica para cargar peliculas
+    pass
+
+def solicitar_genero():
+    """Se le pide al usuariio ingresar un genero favorito"""
+    # Logica para pedir el genero
+    pass
+
+def recomendar_pelicula(peliculas, genero):
+    """Se recomienda una pelicula segun el genero, de forma aleatoria"""
+    # Logica para recomendar pelicula
+    pass
+
+def ejecutar_agente_recomendacion():
+    """Ejecución del agente para recomendar una pelicula"""
+    # Logica para manejar el agente
+    pass
+
+# Se ejecuta el agente de recomendación
+ejecutar_agente_recomendacion()
